a4cfe866e2e46bff7573f5f6062c34fee6373619
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-06-19 16:58:48 +0200

    Add basic styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,32 @@
-<nav><a href=""> Svelte </a>
+<script>
+	import '../styles.css';
+</script>
 
-<div class="links">
-    <a href="/search">Search</a>
-    <a href="/watchlist">Watchlist</a>
-    <a href="/login">Log in</a>
-</div>
+<nav>
+	<a href="/"> SvelteFlix </a>
+
+	<div class="links">
+		<a href="/search">Search</a>
+		<a href="/watchlist">Watchlist</a>
+		<a href="/login">Log in</a>
+	</div>
 </nav>
+
+<style>
+nav {
+    display: flex;
+    width: 100%;
+    height: 3rem;
+    align-items: center;
+    justify-content: space-between;
+    max-width: var(--column);
+    padding: 0 var(--side);
+    color: var(--accent);
+}
+
+a {
+    text-decoration: none;
+    color: inherit;
+}
+
+</style>
